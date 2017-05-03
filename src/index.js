b1f376eb2eab85ac9a3ada9bbe64c0079f38cd3d
Benilyn <benilyn.gonzales@gmail.com>
2017-05-03 12:27:08 -0700

    create and test STANDARD_VALUES for createDeck function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+const STANDARD_VALUES = 'A 2 3 4 5 6 7 8 9 10 J Q K'.split(' ');
+
 function createDeck() {
-	return [
-		createCard('A', 'spades')
-	]; //return createCard
+	const deck = [];
+	STANDARD_VALUES.forEach(function(value) {
+		const card = createCard(value, 'spades');
+		deck.push(card);
+	}); //STANDARD_VALUES.forEach
+	return deck; 
 } //createDeck
 
 function createCard(value, suit) {
--- test/test-deck.js
+++ test/test-deck.js
@@ -6,6 +6,8 @@ const barajas = require('../src/index.js');
 describe('Deck of Cards', function() {
 	it('should get a deck of cards', function() {
 		const deck = barajas.createDeck();
+		console.log(deck);
+		deck.length.should.equal(13);
 		deck.should.be.a('array');	
 	}); //it (should get a deck of cards)
 
@@ -14,5 +16,11 @@ describe('Deck of Cards', function() {
 		const card = barajas.createCard('A', 'spades');
 		deck.should.include(card);
 	}); //it (should have A of spades)
+
+	it('should have K of spades', function() {
+		const deck = barajas.createDeck();
+		const card = barajas.createCard('K', 'spades');
+		deck.should.include(card);
+	}); //it (should have K of spades)
 }); //describe (Deck of Cards)
 
