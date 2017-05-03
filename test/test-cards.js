69631d78bf00fd3968c69aeb6fc62b25ec0abfeb
Benilyn <benilyn.gonzales@gmail.com>
2017-05-03 12:03:07 -0700

    split card test and deck test

--- test/test-cards.js
+++ test/test-cards.js
@@ -3,18 +3,6 @@ const should = chai.should();
 
 const barajas = require('../src/index.js');
 
-describe('Deck of Cards', function() {
-	it('should get a deck of cards', function() {
-		const deck = barajas.createDeck();
-		deck.should.be.a('array');	
-	}); //it (should get a deck of cards)
-
-	it('should have A of spades', function() {
-		const deck = barajas.createDeck();
-		const card = barajas.createCard('A', 'spades');
-		deck.should.include(card);
-	}); //it (should have A of spades)
-}); //describe (Deck of Cards)
 
 describe('Cards', function() {
 	it('should get A of spades', function() {
