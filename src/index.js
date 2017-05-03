3dfab2c678d6acca11dc2a17f79b2d6139e0ea55
Benilyn <benilyn.gonzales@gmail.com>
2017-05-03 12:57:36 -0700

    create STANDARD_SUIT for deck

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const STANDARD_VALUES = 'A 2 3 4 5 6 7 8 9 10 J Q K'.split(' ');
+const STANDARD_SUITS = 'spades hearts diamonds clubs'.split(' ');
 
 function createDeck() {
 	const deck = [];
-	STANDARD_VALUES.forEach(function(value) {
-		const card = createCard(value, 'spades');
-		deck.push(card);
-	}); //STANDARD_VALUES.forEach
+	STANDARD_SUITS.forEach(function(suit) {
+		STANDARD_VALUES.forEach(function(value) {
+			const card = createCard(value, suit);
+			deck.push(card);
+		}); //STANDARD_VALUES.forEach
+	}); //STANDARD_SUITS.forEach
+
+		
 	return deck; 
 } //createDeck
 
